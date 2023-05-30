b68d4d7560eadff3a38b18fea5a0b5cce8aabf23
Phillip Sanchez <plang.psm@gmail.com>
2023-05-29 19:55:47 -0700

    fix projects nav bar bug

--- projects.js
+++ projects.js
@@ -1,6 +1,21 @@
 import projects from './data.js';
 const projectsHTML = document.getElementById('projects');
 
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  })
+);
+
 // Renders projects.
 function renderProjects(data) {
   let html = '';
